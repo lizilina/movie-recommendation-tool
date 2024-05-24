77a659a262b60b8a09ec5da55656f2214abd1a3a
Lizi Jijelava <169905282+lizilina@users.noreply.github.com>
2024-05-24 17:19:40 +0400

    Add files via upload

---
+++ movie_advisor.py
@@ -0,0 +1,60 @@
+import requests
+import json
+import sqlite3
+
+api_key = '690d1758'
+title = input("Enter the title of the movie: ")
+
+url = f"http://www.omdbapi.com/?t={title}&apikey={'690d1758'}"
+response = requests.get(url)
+
+# Using 3-4 functions/attributes of the requests module
+print(f'URL: {r.url}')
+print(f'Status Code: {r.status_code}')
+print(f'Response headers: {r.headers}')
+
+# Gives user the details about the move.
+if response.status_code == 200:
+    movie_details = response.json()
+
+    title = movie_details['Title']
+    year = movie_details['Year']
+    rating = movie_details['imdbRating']
+    runtime = movie_details['Runtime']
+    plot = movie_details['Plot']
+
+    print(f"Title: {title}")
+    print(f"Year: {year}")
+    print(f"Rating: {rating}")
+    print(f"Length: {runtime}")
+    print(f"Description: {plot}")
+
+
+# Gives recommendations based on the rating
+    try:
+        Rating = float(rating)
+        if Rating >= 8.0:
+            print("This movie is highly recommended!")
+        elif Rating < 5.0:
+            print("I do not recommend watching this movie.")
+        else:
+            print("The movie has an avarage rating.")
+    except ValueError:
+        print("Information is not available.")
+
+    # Save movie details to a file
+    with open('movie_details.json', 'w') as json_file:
+        json.dump(movie_details, json_file, indent=4)
+    print("Data saved to movie_details.json")
+
+# # Save movie details to a SQLite database
+    conn = sqlite3.connect('movies.db')
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS movies (title TEXT, year TEXT, rating TEXT, runtime TEXT,plot TEXT)''')
+    cursor.execute('''INSERT INTO movies (title, year, rating, runtime, plot)VALUES (?, ?, ?, ?, ?)''',(title, year, rating, runtime, plot))
+
+    conn.commit()
+    conn.close()
+    print("Movie details saved to database.")
+else:
+    print("Movie not found :(")
